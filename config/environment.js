5104593597ab59b19798d8e270f17dcc2c5a028c
cesar magallon <cesar9mv@gmail.com>
2019-09-01 14:44:22 -0500

    [add]config

--- config/environment.js
+++ config/environment.js
@@ -5,3 +5,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 process.env.URL_DB = process.env.NODE_ENV === 'dev' ? '' : process.env.MYSQL_CONNECTION;
 
 process.env.PASSWORD_DB = '12345678';
+
+process.env.PORT = process.env.PORT || 3000 
