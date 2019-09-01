83084ac4cb8623355d526969ebfaca484d64bcba
cesar magallon <cesar9mv@gmail.com>
2019-09-01 13:15:36 -0500

    [add]sequelize config

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 
-process.env.PASSWORD_DB = '12345678';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 process.env.URL_DB = process.env.NODE_ENV === 'dev' ? '' : process.env.MYSQL_CONNECTION;
+
+process.env.PASSWORD_DB = '12345678';
