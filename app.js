41fbddb099aedbef1e26084b29854a8cff448bf3
cesar magallon <cesar9mv@gmail.com>
2019-09-01 14:43:04 -0500

    [add]listen

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const Express = require('express');
+const App = Express();
 const sequelize = require('./db/index');
+const Routes = require('./routes/routes');
+
+App.use('/api', Routes);
+
 sequelize
 .authenticate()
 .then(() => {
@@ -8,3 +14,7 @@ sequelize
 .catch(err => {
   console.error('Unable to connect to the database:', err);
 });
+
+App.listen(process.env.PORT, () => {
+  console.log(`Escuchando el en puerto ${process.env.PORT}`);
+})
