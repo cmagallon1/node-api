8240e0a82118e974c4b86d878e085563f7cff331
cesar magallon <cesar9mv@gmail.com>
2019-09-10 23:10:41 -0500

    [add]authors crud

---
+++ routes/controllers/authors.js
@@ -0,0 +1,106 @@
+const Express = require('express');
+const App = Express();
+const BodyParser = require('body-parser');
+const { Author } = require('../../db/models/index');
+
+App.use(BodyParser.urlencoded({extended: false}));
+
+App.use(BodyParser.json());
+
+App.get('/', (req, res) => {
+    Author.findAll().then(authors => {
+        return res.json({
+            ok: true,
+            authors
+        });
+    })
+    .catch(err => {
+        return res.status(500).json({
+            ok: false,
+            err
+        });
+    })
+});
+
+App.get('/:id', (req, res) => {
+    let id = req.params.id;
+    Author.findOne({where: {id}}).then(author => {
+        if(!author){
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "No se encuentra el usuario especificado"
+                }
+            });
+        }
+        return res.json({
+            ok: false,
+            author
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({
+            ok: false,
+            err
+        });
+    })
+});
+
+App.post('/', (req, res) => {
+    let body = req.body;
+    let author = {
+        name: body.name,
+        lastname: body.lastname
+    };
+    Author.create(author).then(author => {
+        return res.json({
+            ok: true,
+            author
+        });
+    })
+    .catch(err => {
+        return res.status(400).json({
+            ok: false,
+            err
+        });
+    })
+});
+
+App.put('/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    let author = {
+        name: body.name,
+        lastname: body.lastname
+    };
+    Author.update(author, {where: {id}, returning: true}).then(author => {
+        return res.json({
+            ok: true,
+            author
+        });
+    })
+    .catch(err => {
+        return res.status(400).json({
+            ok: false,
+            err
+        });
+    })
+});
+
+App.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    Author.destroy({where: {id}}).then(author => {
+        return res.json({
+            ok: true,
+            author
+        })
+    })
+    .catch(err => {
+        return res.status(400).json({
+            ok: false,
+            err
+        });
+    });
+});
+
+module.exports = App;
